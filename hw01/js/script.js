d11979ee63bbadf76a689ea6d48ad67448ec6eea
griashiro <griashiro@gmail.com>
2018-03-11 19:29:14 +0300

    1-я и 2-я домашняя работа.

---
+++ hw01/js/script.js
@@ -0,0 +1,137 @@
+const DATA_URL = 'data.json'
+
+fetch(DATA_URL)
+  .then((res) => {
+    res.json().then((data) => renderMockData(data))
+  })
+  .catch((err) => alert('Не удалось загрузить data.json\n' + err))
+
+const renderMockData = (dataJSON) => {
+  const main = document.getElementById('main')
+  main.appendChild(createDocumentFragment(dataJSON))
+}
+
+const createDocumentFragment = (dataJSON) => {
+  const fragment = document.createDocumentFragment()
+
+  dataJSON.map((cardData) => {
+    fragment.appendChild(createCard(cardData))
+  })
+
+  return fragment
+}
+
+const createCard = (cardData) => {
+  const card = createBlock(`card card_size_${cardData.size}`)
+  addTitle(card, cardData.title)
+
+  if (cardData.image) {
+    addImage(card, cardData.image, cardData.size)
+  } else {
+    markAsNoImage(card)
+  }
+
+  addAdditionalContent(card, cardData)
+
+  return card
+}
+
+const addAdditionalContent = (card, cardData) => {
+  const div = createBlock('card__additional-content')
+
+  if (cardData.description) {
+    addDescription(div, cardData.description)
+  }
+
+  addPane(div, cardData)
+
+  card.appendChild(div)
+}
+
+const addPane = (elem, cardData) => {
+  const pane = createBlock('card__pane')
+
+  if (cardData.channelName) {
+    addChannel(pane, cardData.channelName)
+  }
+
+  addButtons(pane)
+
+  elem.appendChild(pane)
+}
+
+const addButtons = (elem) => {
+  elem.appendChild(
+    createBlock('card__dots', '<svg><use xlink:href="#s-dots"/></svg>'))
+
+  elem.appendChild(
+    createBlock('card__heart', '<svg><use xlink:href="#s-heart"/></svg>'))
+}
+
+const addTitle = (elem, title) => {
+  const div = createBlock('card__title-container')
+  div.appendChild(createBlock('card__title', title))
+  elem.appendChild(div)
+}
+
+const addImage = (elem, src, imageSize) => {
+  const div = createBlock('card__picture')
+  div.appendChild(createPicture(src, imageSize))
+  elem.appendChild(div)
+}
+
+const addDescription = (elem, description) => {
+  const gradient = '<div class="card__gradient"></div>'
+  elem.appendChild(createBlock('card__description', gradient + description))
+}
+
+const addChannel = (elem, channelName) => {
+  elem.appendChild(createBlock('card__channel', channelName))
+}
+
+const markAsNoImage = (elem) => {
+  elem.className += ' card_no-image'
+}
+
+const createBlock = (className, text = '') => {
+  const div = document.createElement('div')
+
+  div.className = className
+  div.innerHTML = text
+
+  return div
+}
+
+const createPicture = (src, size) => {
+  const img = document.createElement('img')
+
+  img.src = src
+  img.srcset = getSrcSet(src, size)
+
+  // TODO: добавить рассчет sizes
+
+  img.alt = 'Изображение для карточки'
+
+  return img
+}
+
+const IMAGE_SIZES = {
+  s: [112, 224, 336],
+  m: [172, 344, 516],
+  l: [232, 464, 696]
+}
+
+const getSrcSet = (src, size) => {
+  src = src.split('.')
+
+  const file = src[0]
+  const extension = src[1]
+
+  const imgSize = IMAGE_SIZES[size]
+
+  return `
+    ${file}.${extension} ${imgSize[0]}w,
+    ${file}@2x.${extension} ${imgSize[1]}w,
+    ${file}@3x.${extension} ${imgSize[2]}w
+  `.replace('\n', '')
+}
---
+++ hw02/js/doorOthers.js
@@ -0,0 +1,240 @@
+// ===================== Пример кода первой двери =======================
+/**
+ * @class Door0
+ * @augments DoorBase
+ * @param {Number} number
+ * @param {Function} onUnlock
+ */
+function Door0(number, onUnlock) {
+    DoorBase.apply(this, arguments);
+
+    var buttons = [
+        this.popup.querySelector('.door-riddle__button_0'),
+        this.popup.querySelector('.door-riddle__button_1'),
+        this.popup.querySelector('.door-riddle__button_2')
+    ];
+
+    buttons.forEach(function(b) {
+        b.addEventListener('pointerdown', _onButtonPointerDown.bind(this));
+        b.addEventListener('pointerup', _onButtonPointerUp.bind(this));
+        b.addEventListener('pointercancel', _onButtonPointerUp.bind(this));
+        b.addEventListener('pointerleave', _onButtonPointerUp.bind(this));
+    }.bind(this));
+
+    function _onButtonPointerDown(e) {
+        e.target.classList.add('door-riddle__button_pressed');
+        checkCondition.apply(this);
+    }
+
+    function _onButtonPointerUp(e) {
+        e.target.classList.remove('door-riddle__button_pressed');
+    }
+
+    /**
+     * Проверяем, можно ли теперь открыть дверь
+     */
+    function checkCondition() {
+        var isOpened = true;
+        buttons.forEach(function(b) {
+            if (!b.classList.contains('door-riddle__button_pressed')) {
+                isOpened = false;
+            }
+        });
+
+        // Если все три кнопки зажаты одновременно, то откроем эту дверь
+        if (isOpened) {
+            this.unlock();
+        }
+    }
+}
+
+// Наследуемся от класса DoorBase
+Door0.prototype = Object.create(DoorBase.prototype);
+Door0.prototype.constructor = DoorBase;
+// END ===================== Пример кода первой двери =======================
+
+/**
+ * @class Door1
+ * @augments DoorBase
+ * @param {Number} number
+ * @param {Function} onUnlock
+ */
+function Door1(number, onUnlock) {
+    DoorBase.apply(this, arguments);
+
+    // ==== Напишите свой код для открытия второй двери здесь ====
+    const buttons = this.getButtons(...this.getButtonsClasses());
+    const activeButtons = Array(buttons.length).fill(false);
+    const lockButton = this.getLockButton();
+
+    this.hide(...buttons, lockButton);
+    this.setEventListeners(this, buttons, _onDown, _onMove, _onUp);
+
+    lockButton.addEventListener('pointerup', () => {
+        activeButtons.fill(false);
+        this.unlock();
+    })
+
+    function _onDown(e) {
+        e.target.classList.add('stairs-riddle__button_pressed');
+    }
+
+    function _onMove(e) {
+        const buttonId = Number(e.target.dataset.buttonid);
+        const nextButtonId = buttonId + 1;
+        const isNotLastButton = nextButtonId < buttons.length;
+
+        const classList = e.target.classList;
+
+        if (isActive(e)) {
+            classList.add('stairs-riddle__button_active');
+            activeButtons[buttonId] = true;
+
+            if (isNotLastButton) {
+                buttons[nextButtonId].classList.remove('stairs-riddle__button_hidden');
+            }
+
+            if (checkCondition()) {
+                lockButton.classList.remove('stairs-riddle__button_hidden');
+            }
+        } else {
+            classList.remove('stairs-riddle__button_active');
+            activeButtons[buttonId] = false;
+
+            if (isNotLastButton) {
+                buttons[nextButtonId].classList.add('stairs-riddle__button_hidden');
+            }
+
+            lockButton.classList.add('stairs-riddle__button_hidden');
+        }
+    }
+
+    function _onUp(e) {
+        this.diactive(...buttons);
+        activeButtons[e.target.dataset.buttonid] = false;
+        this.collapse(...buttons);
+        this.hide(...buttons, lockButton);
+    }
+
+    function isActive(e) {
+        const offsetThreshold = 40;
+        const dataset = e.target.dataset;
+
+        const isTopButton = dataset.buttonid == 0 || dataset.buttonid == 2
+
+        const rect = e.target.getBoundingClientRect();
+
+        if (isTopButton) {
+            return e.clientY < rect.top + offsetThreshold;
+        } else {
+            return e.clientY > rect.top + rect.height - offsetThreshold;
+        }
+    }
+
+    function checkCondition() {
+        return activeButtons.every((isActive) => {
+            return isActive;
+        })
+    }
+    // ==== END Напишите свой код для открытия второй двери здесь ====
+}
+
+Door1.prototype = Object.create(DoorBase.prototype);
+Door1.prototype.constructor = DoorBase;
+
+Door1.prototype.getButtons = function () {
+    const buttons = [];
+
+    for (var i = 0, len = arguments.length; i < len; ++i) {
+        buttons.push(this.popup.querySelector(arguments[i]));
+    }
+
+    return buttons;
+}
+
+Door1.prototype.getLockButton = function () {
+    return this.popup.querySelector('.lock__picture');
+}
+
+Door1.prototype.getButtonsClasses = function () {
+    const BUTTONS_COUNT = 4;
+    const buttonsNames = [];
+
+    for (let i = 0, len = BUTTONS_COUNT; i < len; ++i) {
+        buttonsNames.push('.stairs-riddle__button_' + i);
+    }
+
+    return buttonsNames;
+}
+
+Door1.prototype.setEventListeners = function (self, buttons, onDown, onMove, onUp) {
+    buttons.forEach((b) => {
+        b.addEventListener('pointerdown', onDown.bind(self));
+        b.addEventListener('pointermove', onMove.bind(self));
+        b.addEventListener('pointerleave', onUp.bind(self));
+        b.addEventListener('pointerup', onUp.bind(self));
+        b.addEventListener('pointercancel', onUp.bind(self));
+    })
+}
+
+Door1.prototype.hide = function () {
+    const SECOND_BUTTON = 1;
+    for (let i = SECOND_BUTTON, len = arguments.length; i < len; ++i) {
+        arguments[i].classList.add('stairs-riddle__button_hidden');
+    }
+}
+
+Door1.prototype.collapse = function () {
+    for (let i = 0, len = arguments.length; i < len; ++i) {
+        arguments[i].classList.remove('stairs-riddle__button_pressed');
+    }
+}
+
+Door1.prototype.diactive = function () {
+    for (let i = 0, len = arguments.length; i < len; ++i) {
+        arguments[i].classList.remove('stairs-riddle__button_active');
+    }
+}
+
+/**
+ * @class Door2
+ * @augments DoorBase
+ * @param {Number} number
+ * @param {Function} onUnlock
+ */
+function Door2(number, onUnlock) {
+    DoorBase.apply(this, arguments);
+
+    // ==== Напишите свой код для открытия третей двери здесь ====
+    // Для примера дверь откроется просто по клику на неё
+    this.popup.addEventListener('click', function() {
+        this.unlock();
+    }.bind(this));
+    // ==== END Напишите свой код для открытия третей двери здесь ====
+}
+Door2.prototype = Object.create(DoorBase.prototype);
+Door2.prototype.constructor = DoorBase;
+
+/**
+ * Сундук
+ * @class Box
+ * @augments DoorBase
+ * @param {Number} number
+ * @param {Function} onUnlock
+ */
+function Box(number, onUnlock) {
+    DoorBase.apply(this, arguments);
+
+    // ==== Напишите свой код для открытия сундука здесь ====
+    // Для примера сундук откроется просто по клику на него
+    this.popup.addEventListener('click', function() {
+        this.unlock();
+    }.bind(this));
+    // ==== END Напишите свой код для открытия сундука здесь ====
+
+    this.showCongratulations = function() {
+        alert('Поздравляю! Игра пройдена!');
+    };
+}
+Box.prototype = Object.create(DoorBase.prototype);
+Box.prototype.constructor = DoorBase;
