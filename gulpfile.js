dccc36cb25e3b6f54125615ccdb7cdfc02af5033
griashiro <griashiro@gmail.com>
2018-03-12 10:48:00 +0300

    Перезд на post.css и gulp.

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var nested = require('postcss-nested');
+
+gulp.task('postcss', function () {
+    var plugins = [
+      autoprefixer({browsers: ['last 1 version']}),
+      nested
+    ];
+    return gulp.src('src/style.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('style.css', ['postcss']);
+});
