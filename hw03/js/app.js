1cac99357baaf6f8786bedcd769b98da006e8fca
griashiro <griashiro@gmail.com>
2018-03-19 09:34:17 +0300

    Доработки домашнего задания №3.

--- hw03/js/app.js
+++ hw03/js/app.js
@@ -3,13 +3,21 @@ import T800Interface from './T800Interface.js'
 
 const video = document.createElement('video');
 const canvas = document.getElementById("canvas");
-
 const terminatorVision = new TerminatorVision(canvas, video);
 
-let requestID;
+const t800Interface = new T800Interface()
+
+const audioContext = new AudioContext();
+const analyser = audioContext.createAnalyser();
+
+let requestID, source, dataArray, bufferLength;
+
+const sinewave = document.getElementById("sinewave")
+const sinewaveCtx = sinewave.getContext("2d");
 
 function render(time) {
   terminatorVision.drawFrame();
+  drawSoundAnalyzer();
   requestID = requestAnimationFrame(render);
 }
 
@@ -22,12 +30,15 @@ function successCallback (stream) {
     video.srcObject = stream;
     video.muted = true;
 
+    conntectSoundAnalyzerTo(stream)
+
     video.onloadedmetadata = function(e) {
       video.play();
     };
 
     video.onplaying = function() {
       requestID = requestAnimationFrame(render);
+      createTerminatorInterface()
       changeTitleText('play');
     }
 
@@ -35,10 +46,52 @@ function successCallback (stream) {
       cancelAnimationFrame(requestID);
       changeTitleText('pause');
     }
+}
 
-    function changeTitleText (status) {
-      document.title = `Multimedia: ${status.toUpperCase()}`
+function changeTitleText (status) {
+  document.title = `Multimedia: ${status.toUpperCase()}`
+}
+
+function conntectSoundAnalyzerTo(stream) {
+  source = audioContext.createMediaStreamSource(stream);
+  source.connect(analyser);
+  analyser.fftSize = 2048;
+
+  bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+}
+
+function drawSoundAnalyzer() {
+  const width = sinewave.width;
+  const height = sinewave.height;
+
+  analyser.getByteTimeDomainData(dataArray);
+
+  sinewaveCtx.clearRect(0, 0, width, height);
+
+  sinewaveCtx.lineWidth = 2;
+  sinewaveCtx.strokeStyle = 'rgb(255, 255, 255)';
+
+  sinewaveCtx.beginPath();
+
+  const sliceWidth = width * 1.0 / bufferLength;
+  let x = 0;
+
+  for(let i = 0; i < bufferLength; ++i) {
+      const v = dataArray[i] / 128.0;
+      const y = v * height / 2;
+
+      if (i === 0) {
+        sinewaveCtx.moveTo(x, y);
+      } else {
+        sinewaveCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
     }
+
+  sinewaveCtx.lineTo(canvas.width, canvas.height / 2);
+  sinewaveCtx.stroke();
 }
 
 function errorCallback (error) {
@@ -53,50 +106,13 @@ canvas.addEventListener('click', () => {
   video.paused ? video.play() : video.pause();
 });
 
-function runTerminatorInterface () {
-  setTimeout(() => {
-      createScanner()
-  }, 2000)
-  setInterval(() => {
-    createDigitsInfo()
-  }, 3000)
-}
+function createTerminatorInterface () {
+  const analyserElem = document.querySelector('.analyser');
+  const scannerElem = document.querySelector('.scanner');
 
-function createDigitsInfo () {
-  const container = document.querySelector('.digits-info');
+  t800Interface.createAnalyser(analyserElem)
+  analyserElem.classList.add('showtext_animate')
 
-  container.innerHTML = ''
+  t800Interface.createScanner(scannerElem)
 
-  const t800Interface = new T800Interface()
-  t800Interface.createDigitsInfo(container)
-
-  const childElems = [...container.children];
-  childElems.reverse();
-
-  makeVisible(childElems.pop());
-  makeVisible(childElems.pop());
-
-  const showText = () => {
-    if (childElems.length > 0) {
-      setTimeout(() => {
-        makeVisible(childElems.pop());
-        showText();
-      }, 100)
-    }
-  }
-
-  showText();
-}
-
-function makeVisible (elem) {
-  elem.style.visibility = 'visible'
-}
-
-function createScanner() {
-  const container = document.querySelector('.scanner');
-
-  const t800Interface = new T800Interface()
-  t800Interface.createScanner(container)
-}
-
-runTerminatorInterface()
+};
