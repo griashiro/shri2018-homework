5c7c0128c8e66a6af78dd76ffbd9928c99c21fe1
griashiro <griashiro@gmail.com>
2018-03-20 17:08:11 +0300

    Статика.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
@@ -5,6 +6,8 @@ const config = require('./config')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.redirect('/branches')
