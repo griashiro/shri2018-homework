8bac45290673983ea041d6a080dea4a8b414d4c9
griashiro <griashiro@gmail.com>
2018-03-20 19:56:35 +0300

    Обработка 404 и 500 ошибок.

--- app.js
+++ app.js
@@ -6,17 +6,27 @@ const treeRoute = require('./routes/tree')
 const branchRoute = require('./routes/branch')
 const commitRoute = require('./routes/commit')
 
+const notFoundRoute = require('./routes/not-found')
+
 const config = require('./config')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.redirect('/branches')
 })
 
-app.use('/branches', treeRoute, branchRoute, commitRoute)
+app.use('/branches', treeRoute, branchRoute, commitRoute, notFoundRoute)
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err)
+  }
+
+  res.send('500', 500)
+})
 
 app.listen(config.PORT)
