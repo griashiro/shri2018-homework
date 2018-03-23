65a552e20c3711d3458b4e0b9538e4bff2eae6e5
griashiro <griashiro@gmail.com>
2018-03-23 18:04:03 +0300

    Исправлена ошибка в роутинге.

--- helpers/get-path.js
+++ helpers/get-path.js
@@ -1,11 +1,11 @@
 const urlencode = require('urlencode')
 
-module.exports = (path) => {
-    path = urlencode.decode(path) || ''
+module.exports = (path = '') => {
+  path = urlencode.decode(path) || ''
 
-    if (path[0] === '/') {
-      path = '.' + path
-    }
+  if (path[0] === '/') {
+    path = '.' + path
+  }
 
-    return path
+  return path
 }
